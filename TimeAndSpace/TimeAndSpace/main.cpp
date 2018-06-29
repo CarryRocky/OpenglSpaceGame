6753e84ce694cc7c16428b1045c86084d2c8f8d7
CarryRocky <32424253+CarryRocky@users.noreply.github.com>
2018-06-29 15:56:03 -0700

    setting up done

---
+++ TimeAndSpace/TimeAndSpace/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TimeAndSpace
+//
+//  Created by Carry on 2018/6/29.
+//  Copyright © 2018 Carry. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
